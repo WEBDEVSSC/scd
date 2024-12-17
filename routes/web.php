34ee9bfc4a9be5b17f7624913f1135ce7d5dcccc
WEB DEV SSC <soportewebssc@gmail.com>
2024-12-16 23:41:32 -0600

    CORRECCION HOME

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/', function () {
 
 Auth::routes(['register' => false]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 /*******************************************************************************************
  * 
